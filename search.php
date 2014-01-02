df32220586b4c700d3d65b533cf6164d1e185f73
jamesread <jread@redhat.com>
2014-01-02 01:11:17 +0000

    Server side generated results.

--- search.php
+++ search.php
@@ -1,28 +1,13 @@
 <?php
 
+require_once 'common.php';
+
 function outputJson($d) {
 	header('Content-Type: application/json');
 	echo json_encode($d);
 	exit;
 }
-
-require_once 'libAllure/Database.php';
-
-use \libAllure\DatabaseFactory;
-use \libAllure\Database;
-
-$db = new Database();
-
-function getObjects() {
-	global $db;
-
-	$sql = 'SELECT o.title FROM objects o ';
-	$stmt = $db->prepare($sql);
-	$stmt->execute();
-
-	return $stmt->fetchAll();
-}
-
-outputJson(getObjects());
+ 
+outputJson(getObjects($_REQUEST['term']));
 
 ?>
