5949d8b02dff15e3f15be5d072458e7a90afe0fe
jamesread <jread@redhat.com>
2014-01-02 16:12:13 +0000

    Database view is now so much nicer.

--- viewDatabase.php
+++ viewDatabase.php
@@ -8,8 +8,28 @@ $sql = 'SELECT o.id, o.title FROM objects o ORDER BY o.title ASC ';
 $stmt = $db->prepare($sql);
 $stmt->execute();
 
+echo '<table>';
+echo '<tr><th colspan = "2">id</th><th>title</th><th>types</th><th>provides</th></tr>';
 foreach (getObjects() as $object) {   
-	echo $object['id'] . ': <img src = "icons/' . $object['icon'] . '" /> ' . $object['title'] . ' types: [' . $object['types'] . '] provides: [' . $object['provides'] . ']<br />';
+	echo '<tr><td>' . $object['id'] . '</td><td><img src = "icons/' . $object['icon'] . '" /></td><td>' . $object['title'] . '</td><td>[' . $object['types'] . ']</td><td>[' . $object['provides'] . ']</td></tr>';
 }
+echo '</table>';
 
 ?>
+<style type = "text/css">
+tr:hover td {
+	background-color: cyan;
+}
+
+td {
+	padding: .2em;
+}
+
+table {
+	width: 100%;
+}
+
+th {
+	text-align: left;
+}
+</style>
